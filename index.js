e342527a163b2d7eed863cb774d66580e27aac32
Ahmed Farrel Hegry <86486909+Farrel-Hegry@users.noreply.github.com>
2024-06-08 22:20:22 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+import UserRoute from "./routes/UserRoute.js";
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(UserRoute);
+
+app.listen(PORT, ()=> console.log(`Berhasil berjalan pada PORT ${PORT}`));
